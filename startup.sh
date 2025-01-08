09b269200c52dc5d2a857cc141f2e5f00365446e
jithinjohnkaniyampoikayil <jithin.john537@gmail.com>
2025-01-08 16:09:38 +0000

    feat: add custom Nginx configuration and update Node.js API path in startup script

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Copy custom nginx configuration file to the default location
+cp /home/site/wwwroot/nginx-azure.conf /etc/nginx/conf.d/default.conf
+
 # Start Nginx
 nginx -g 'daemon off;' &
 
 # Start Node.js API
-node /home/site/wwwroot/src/server.js
+node /home/site/wwwroot/api/src/server.js
