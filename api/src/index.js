8ef6f6c8d2650b4c1aee1c4ebf4433c4d12ff329
jithinjohnkaniyampoikayil <jithin.john537@gmail.com>
2025-01-11 01:10:58 +0000

    fix: update main entry point to src/index.js and remove old server.js file

--- api/src/index.js
+++ api/src/index.js
@@ -6,7 +6,17 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const server = http.createServer(app);
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ message: "Hello, Jithin" }));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Not Found\n");
+  }
+});
 const io = socketIo(server, {
   cors: {
     origin: "https://jjlba.azurewebsites.net/",
