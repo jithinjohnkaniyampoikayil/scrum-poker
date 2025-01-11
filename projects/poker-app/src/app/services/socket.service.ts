a5d5fc05c6fc7d9fc45de28fbcae5de25f52fabb
jithinjohnkaniyampoikayil <jithin.john537@gmail.com>
2025-01-11 15:22:52 +0000

    fix: update socket connection URL to point to new server

--- projects/poker-app/src/app/services/socket.service.ts
+++ projects/poker-app/src/app/services/socket.service.ts
@@ -9,7 +9,7 @@ export class SocketService {
   private socket: Socket;
 
   constructor() {
-    this.socket = io('https://jjlba.azurewebsites.net');
+    this.socket = io('https://jjwfnode.azurewebsites.net');
   }
 
   connect() {
